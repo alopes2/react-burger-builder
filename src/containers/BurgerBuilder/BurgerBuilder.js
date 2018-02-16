22f51f535ab38351f4e247d7cc9dc95b39be2b9a
alopes2 <andre.lopes@exxonmobil.com>
2018-02-16 12:24:48 -0200

    passing query params to checkout

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -123,7 +123,17 @@ class BurgerBuilder extends Component {
         //         console.log(error);
         //         this.setState({loading: false, purchasing: false});
         //     });
-        this.props.history.push('/checkout');
+
+        const queryParams = [];
+
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+        }
+
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryParams.join('&')
+        });
     };
 
     render () {
