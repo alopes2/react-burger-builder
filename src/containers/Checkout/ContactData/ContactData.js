06625ce883331a46cffcb001e72881aecde50f0f
alopes2 <andre.lopes@exxonmobil.com>
2018-02-20 10:50:27 -0300

    creating and using custom input element

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -5,7 +5,7 @@ import axios from '../../../axios-orders';
 
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
-
+import Input from '../../../components/UI/Input/Input';
 import classes from './ContactData.css';
 
 class ContactData extends Component {
@@ -55,10 +55,10 @@ class ContactData extends Component {
             <React.Fragment>
                 <h4>Enter your Contact Data</h4>
                 <form>
-                    <input className={classes.Input} type="text" name="name" placeholder="Your name" />
-                    <input className={classes.Input} type="email" name="email" placeholder="Your email" />
-                    <input className={classes.Input} type="text" name="street" placeholder="Your street" />
-                    <input className={classes.Input} type="text" name="postal" placeholder="Your postal code" />
+                    <Input inputtype="input" type="text" name="name" placeholder="Your name" />
+                    <Input inputtype="input" type="email" name="email" placeholder="Your email" />
+                    <Input inputtype="input" type="text" name="street" placeholder="Your street" />
+                    <Input inputtype="input" type="text" name="postal" placeholder="Your postal code" />
                     <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
                 </form>
             </React.Fragment>
