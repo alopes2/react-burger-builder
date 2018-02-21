b17aba2767d38a01c9f1605985975c53f0128852
alopes2 <andre.lopes@exxonmobil.com>
2018-02-21 10:42:36 -0300

    adding input error messages

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -184,6 +184,7 @@ class ContactData extends Component {
                             invalid={!formElement.config.valid}
                             shouldValidate={formElement.config.validation}
                             touched={formElement.config.touched}
+                            errorMessage={'Please, enter a valid ' + formElement.id}
                             changed={(event) => this.inputChangedHandler(event, formElement.id)}/>
                     ))}
                     <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
