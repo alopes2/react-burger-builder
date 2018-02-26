1889e82a825fdac260b7e6107b2849b1a9ddf16e
alopes2 <andre.lopes@exxonmobil.com>
2018-02-26 10:18:33 -0300

    fixing the purchasable state (removing it)

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -19,7 +19,6 @@ class BurgerBuilder extends Component {
     //     this.state = {...}
     // }
     state = {
-        purchasable: false,
         purchasing: false,
         loading: false,
         error: null
@@ -43,7 +42,7 @@ class BurgerBuilder extends Component {
                 return ingredients[igKey];
             })
             .reduce((sum, el) => sum + el, 0);
-        this.setState({purchasable: sum > 0});
+        return sum > 0;
     }
 
     // addIngredientHandler = (type) => {
@@ -133,7 +132,7 @@ class BurgerBuilder extends Component {
                         ingredientAdded={this.props.onIngredientAdded}
                         ingredientRemoved={this.props.onIngredientRemoved}
                         disabled={disabledInfo}
-                        purchasable={this.state.purchasable}
+                        purchasable={this.updatePurchaseState(this.props.ings)}
                         price={this.props.price}
                         ordered={this.purchaseHandler}/> 
                     </React.Fragment>);
