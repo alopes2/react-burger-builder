8f9270e95d18d1f8a70a81f742be982d11b37a17
alopes2 <andre.lopes@exxonmobil.com>
2018-03-02 09:07:15 -0300

    Redirecting to root when there are no ingredients

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
@@ -35,20 +35,27 @@ class Checkout extends Component {
     }
 
     render() {
+        let summary = <Redirect to="/" />;
+
+        if(this.props.ings) {
+            summary = (
+                <div>
+                    <CheckoutSummary 
+                        ingredients={this.props.ings}
+                        checkoutCancelled={this.checkoutCancelledHandler}
+                        checkoutContinued={this.checkoutContinuedHandler}
+                        />
+                    <Route 
+                        path={this.props.match.path + '/contact-data'} 
+                        component={ContactData}/> 
+                        {/* render={() => (<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice}/>)}/>  */}
+                        {/* render={(props) => (<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props}/>)}/>  */}
+                </div>
+            );
+        }
+
         return(
-            <div>
-                <CheckoutSummary 
-                    ingredients={this.props.ings}
-                    checkoutCancelled={this.checkoutCancelledHandler}
-                    checkoutContinued={this.checkoutContinuedHandler}
-                    />
-                <Route 
-                    path={this.props.match.path + '/contact-data'} 
-                    component={ContactData}/> 
-                    {/* //render={() => (<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice}/>)}/>  */}
-                    {/* //render={(props) => (<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props}/>)}/>  */}
-                    
-            </div>
+                {summary}
         );
     }
 }
