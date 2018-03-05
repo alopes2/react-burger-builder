939b3219cf8763d58157afabb0f451c42dd11a69
alopes2 <andre.lopes@exxonmobil.com>
2018-03-05 13:40:24 -0300

    feedback features for auth form

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -6,6 +6,7 @@ import classes from './Auth.css';
 
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 class Auth extends Component {
     state = {
@@ -105,7 +106,7 @@ class Auth extends Component {
             });
         }
 
-        const form = formElements.map(formElement => (
+        let form = formElements.map(formElement => (
             <Input
                 key={formElement.id}
                 elementType={formElement.config.elementType}
@@ -119,8 +120,19 @@ class Auth extends Component {
             />
         ));
 
+        if(this.props.loading)
+            form = <Spinner />
+
+        let errorMessage = null;
+        if(this.props.error) {
+            errorMessage = (
+                <p>{this.props.error.message}</p>
+            );
+        }
+
         return(
             <div className={classes.Auth}>
+                {errorMessage}
                 <form onSubmit={this.submitHandler}>
                     {form}
                     <Button btnType="Success">{this.state.isSignup ? 'SIGN UP' : 'SIGN IN'}</Button>
@@ -140,4 +152,11 @@ const mapDispatchToProps = dispatch => {
     };
 };
 
-export default connect(null, mapDispatchToProps)(Auth);
+const mapStateToProps = state => {
+    return {
+        loading: state.auth.loading,
+        error: state.auth.error
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
