b7f93b93fbdb01914dff60ac28c781f634a8454f
alopes2 <andre.lopes@exxonmobil.com>
2018-03-06 09:58:01 -0300

    handling button and redirecting to auth page when trying to purchase when not authenticated

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -73,7 +73,11 @@ class BurgerBuilder extends Component {
     // }
 
     purchaseHandler = () => {
-        this.setState({purchasing: true});
+        if (this.props.isAuthenticated) {
+            this.setState({purchasing: true});
+        } else {
+            this.props.history.push('/auth');
+        }
     }
 
     purchaseCancelHandler = () => {
@@ -123,7 +127,8 @@ class BurgerBuilder extends Component {
                         disabled={disabledInfo}
                         purchasable={this.updatePurchaseState(this.props.ings)}
                         price={this.props.price}
-                        ordered={this.purchaseHandler}/> 
+                        ordered={this.purchaseHandler}
+                        isAuth={this.props.isAuthenticated}/> 
                     </React.Fragment>);
             orderSummary = <OrderSummary 
                     ingredients={this.props.ings}
@@ -151,7 +156,8 @@ const mapStateToProps = state => {
     return {
         ings: state.burgerBuilder.ingredients,
         price: state.burgerBuilder.totalPrice,
-        error: state.burgerBuilder.error
+        error: state.burgerBuilder.error,
+        isAuthenticated: state.auth.token !== null
     };
 };
 
