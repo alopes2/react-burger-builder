998114ea91438eb7a5df9d9ebb0d12129b7b7540
Andre Lopes <andrevitorlopes@gmail.com>
2018-03-07 18:20:59 -0300

    adding basename

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ const store = createStore(rootReducer, composeEnhancers(
 
 const app = (
     <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename="/">
             <App />
         </BrowserRouter>
     </Provider>
